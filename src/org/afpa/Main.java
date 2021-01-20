06404f4f432477605c7ca27b35cde0da7f147cd8
Germain SIPIERE <gsipiere@gmail.com>
2021-01-20 13:42:52 +0100

    add functions

--- src/org/afpa/Main.java
+++ src/org/afpa/Main.java
@@ -3,36 +3,17 @@ package org.afpa;
 import java.util.Scanner;
 
 public class Main {
-
+    // TODO: factorisons tout ça!
     public static void main(String[] args) {
         //Les variables
-        Scanner sc = new Scanner(System.in);
-        int nbLignes;
-        String mot;
-        int nbVoyelles = 0;
-        String VOYELLES = "aeiouy";
 
-        System.out.println("=========================================");
-        System.out.println("|      Commençons par un triangle       |");
-        System.out.println("=========================================");
-        System.out.println("Quel taille?");
-        nbLignes = sc.nextInt();
-        //affichage du triangle
-        for (int i = 1; i < nbLignes; i++) {
-            System.out.println(" ".repeat(nbLignes - i) +"*".repeat(2 * i - 1));
-        }
-        System.out.println("===========================================");
-        System.out.println("| Maintenant je vais compter les voyelles |");
-        System.out.println("===========================================");
-        System.out.println("Entrez un mot: ");
-        //réglons le problème avec le Scanner
-        sc.nextLine();
-        mot = sc.nextLine().toLowerCase();
-        for (char a : mot.toCharArray()) {
-            nbVoyelles = (VOYELLES.indexOf(a) > 0) ? (nbVoyelles + 1) : nbVoyelles;
-        }
 
-        System.out.printf("Il y a %d voyelle(s) dans %s.",nbVoyelles,mot);
+        afficheTriangle();
+
+        compteVoyelles();
+
+        compteVoyelles();
+
 
         System.out.println("===========================================");
         System.out.println("|            Merci et à bientôt           |");
@@ -40,4 +21,40 @@ public class Main {
 
 
     }
+
+    public static void afficheTriangle(){
+        Scanner sc = new Scanner(System.in);
+        int nbLignes;
+
+        System.out.println("=========================================");
+        System.out.println("|      Commençons par un triangle       |");
+        System.out.println("=========================================");
+        System.out.println("Quel taille?");
+        nbLignes = sc.nextInt();
+        //affichage du triangle
+        for (int i = 1; i < nbLignes+1; i++) {
+            System.out.println(" ".repeat(nbLignes - i) +"*".repeat(2 * i - 1));
+        }
+    }
+
+    public static void compteVoyelles(){
+        Scanner sc = new Scanner(System.in);
+
+        String mot;
+        int nbVoyelles = 0;
+        String VOYELLES = "aeiouy";
+
+        System.out.println("===========================================");
+        System.out.println("| Maintenant je vais compter les voyelles |");
+        System.out.println("===========================================");
+        System.out.println("Entrez un mot: ");
+        mot = sc.nextLine().toLowerCase();
+        for (char a : mot.toCharArray()) {
+            nbVoyelles = (VOYELLES.indexOf(a) >= 0) ? (nbVoyelles+1) : nbVoyelles;
+        }
+        System.out.printf("Il y a %d voyelle(s) dans %s.\n",nbVoyelles,mot);
+    }
+
+
+
 }
