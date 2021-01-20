c4af604d2e81633a165884fa9cea4ebe87d18ba4
Germain SIPIERE <gsipiere@gmail.com>
2021-01-20 15:06:36 +0100

    add packages and objects

--- src/org/afpa/Main.java
+++ src/org/afpa/Main.java
@@ -1,5 +1,7 @@
 package org.afpa;
 
+import org.afpa.tool.Exos;
+
 import java.util.Scanner;
 
 public class Main {
@@ -8,11 +10,11 @@ public class Main {
         //Les variables
 
 
-        afficheTriangle();
+        Exos.afficheTriangle();
 
-        compteVoyelles();
+        Exos.compteVoyelles();
 
-        compteVoyelles();
+        Exos.compteVoyelles();
 
 
         System.out.println("===========================================");
@@ -22,38 +24,9 @@ public class Main {
 
     }
 
-    public static void afficheTriangle(){
-        Scanner sc = new Scanner(System.in);
-        int nbLignes;
 
-        System.out.println("=========================================");
-        System.out.println("|      Commençons par un triangle       |");
-        System.out.println("=========================================");
-        System.out.println("Quel taille?");
-        nbLignes = sc.nextInt();
-        //affichage du triangle
-        for (int i = 1; i < nbLignes+1; i++) {
-            System.out.println(" ".repeat(nbLignes - i) +"*".repeat(2 * i - 1));
-        }
-    }
 
-    public static void compteVoyelles(){
-        Scanner sc = new Scanner(System.in);
 
-        String mot;
-        int nbVoyelles = 0;
-        String VOYELLES = "aeiouy";
-
-        System.out.println("===========================================");
-        System.out.println("| Maintenant je vais compter les voyelles |");
-        System.out.println("===========================================");
-        System.out.println("Entrez un mot: ");
-        mot = sc.nextLine().toLowerCase();
-        for (char a : mot.toCharArray()) {
-            nbVoyelles = (VOYELLES.indexOf(a) >= 0) ? (nbVoyelles+1) : nbVoyelles;
-        }
-        System.out.printf("Il y a %d voyelle(s) dans %s.\n",nbVoyelles,mot);
-    }
 
 
 
---
+++ src/org/afpa/UI/Menu.java
@@ -0,0 +1,8 @@
+package org.afpa.UI;
+
+public class Menu {
+    public void init(){
+        System.out.println();
+    }
+
+}
